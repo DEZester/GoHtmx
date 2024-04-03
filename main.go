f18825ac8ae8c2798985e594b463124949c553b3
DEZester <aromudez@gmail.com>
2024-04-03 21:05:29 +0300

    Adding context data to template

--- main.go
+++ main.go
@@ -6,10 +6,22 @@ import (
 	"net/http"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
 	}
 	http.HandleFunc("/", h1)
 
